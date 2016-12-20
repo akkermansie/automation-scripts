34a2f55b6a00ca279fce84ceb86def2056b2eeac
akkermansie <arif_akkermans@hotmail.com>
2016-12-20 18:27:24 +0100

    update man.py

--- man.py
+++ man.py
@@ -22,3 +22,30 @@ else:
     cur.commit()
     cur.close()
     print ("Table created")
+    
+# class Monitoring():
+#     ''''This is the management script for the monitoring application'''
+# 
+#     def db_connection(self):
+#         ''''Set the database connection'''
+#         self.conn = pyodbc.connect('DRIVER={SQL Server};SERVER=automation-maf.database.windows.net,1433', user='marif@automation-maf', password='P@ssw0rd', database='monitoring')
+#         self.cur = self.conn.cursor
+# 
+#     def check_table(self):
+#          ''''Set the database Table'''    
+#         if self.cur.tables(table='monitor', tableType ='TABLE').fetchone():
+#             print ("Table exists")
+#         else:
+#             data = ("""
+#                               CREATE TABLE monitor
+#                               (
+#                               Hostname varchar(255),
+#                               CPU varchar(255),
+#                               HDD varchar(255),
+#                               Network varchar(255),
+#                               Memory varchar(255),
+#                               )
+#                               """)
+#             self.cur.execute(data)
+#             self.cur.commit()
+#             self.cur.close()
