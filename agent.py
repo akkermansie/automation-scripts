488e630b9b4de8a6bb7304e3726f052f870697bf
akkermansie <arif_akkermans@hotmail.com>
2016-12-21 15:34:10 +0100

    update agent.py

--- agent.py
+++ agent.py
@@ -1,35 +1,19 @@
-import pyodbc, sys, psutil
+import pyodbc, psutil, socket, datetime
 
 con = pyodbc.connect('DRIVER={SQL Server};SERVER=automation-maf.database.windows.net,1433', user='marif@automation-maf', password='P@ssw0rd', database='monitoring')
 c=con.cursor()
 
-def function():
-    pass
     # if sys.platform.startswith('linux'):
     # elif sys.platform.startswith('win32'):
 
-#cpu percentage 5 samples with 1 sec interva;
-def cpu_usage():
-    for x in range(5):
+def mon_loop():
+    for x in range(1):
+        hostname = (socket.gethostname())
         cpu = (psutil.cpu_percent(interval=1))
-        print ('Cpu:',cpu,'% used')
-cpu_usage()
-
-#memory percentage
-def mem_usage():
-    for x in range(1):
-        memory = (str(psutil.virtual_memory().percent))
-        print ('Memory: '+memory+'% used')
-mem_usage()
-
-#disk percentage
-def disk_usage():
-    for x in range(1):
-        disk = str(psutil.disk_usage('/').percent)
-        print ('Disk: ',disk+'% used')
-disk_usage()
-
-#network percentage
-def network_usage():
-    pass
-#wat gaan we monitoren??
+        memory = (psutil.virtual_memory().percent)
+        disk = (psutil.disk_usage('/').percent)
+        date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        c.execute("INSERT into monitor values(?,?,?,?,?)",hostname,cpu,memory,disk,date)
+mon_loop()
+#c.execute("DROP table monitor")
+c.commit()
