d9b5d5890984ccffd959ae73c3d67ebe577d3d82
akkermansie <arif_akkermans@hotmail.com>
2016-12-21 15:56:52 +0100

    update agent.py

--- agent.py
+++ agent.py
@@ -1,7 +1,13 @@
-import pyodbc, psutil, socket, datetime
+try:
+    import pyodbc, psutil, socket, datetime, time
+except ImportError:
+    raise ImportError("Packages could not be imported")
 
-con = pyodbc.connect('DRIVER={SQL Server};SERVER=automation-maf.database.windows.net,1433', user='marif@automation-maf', password='P@ssw0rd', database='monitoring')
-c=con.cursor()
+try:
+    con = pyodbc.connect('DRIVER={SQL Server};SERVER=automation-maf.database.windows.net,1433', user='marif@automation-maf', password='P@ssw0rd', database='monitoring')
+    c=con.cursor()
+except Exception as db_connection:
+    print("Er gaat iets mis. Foutmelding: %s" %(db_connection))
 
     # if sys.platform.startswith('linux'):
     # elif sys.platform.startswith('win32'):
@@ -14,6 +20,9 @@ def mon_loop():
         disk = (psutil.disk_usage('/').percent)
         date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         c.execute("INSERT into monitor values(?,?,?,?,?)",hostname,cpu,memory,disk,date)
-mon_loop()
+        c.commit()
+        time.sleep(30)
 #c.execute("DROP table monitor")
-c.commit()
+
+while True:
+    mon_loop()
