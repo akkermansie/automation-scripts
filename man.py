33b8d1d73205575e69ec873d3e0215fba9616e94
Marco Ariens <marcoariens@gmail.com>
2016-12-23 11:48:31 +0100

    Another BIG change

--- man.py
+++ man.py
@@ -27,22 +27,65 @@ def create_table():
 # data = cur.execute("SELECT hostname, avg(cpu), avg(Memory), avg(HDD) from monitor GROUP BY Hostname").fetchall()
 # print (data)
 
-def create_list():
-    cur.execute("SELECT DISTINCT hostname from monitor")
-    host_data = cur.fetchall()
-    host_list = []
-    for index in range(len(host_data)):
-        host_list.append(host_data[index][0])
-    return (host_list)
+# def create_list():
+#     cur.execute("SELECT DISTINCT hostname from monitor")
+#     host_data = cur.fetchall()
+#     host_list = []
+#     for index in range(len(host_data)):
+#         host_list.append(host_data[index][0])
+#     return (host_list)
+#
+#
+# def get_clients_data():
+#     hostnames = create_list()
+#     for hostname in hostnames:
+#         data = cur.execute("SELECT Hostname, CPU, Memory, HDD from monitor WHERE Hostname='%s'"%(hostname)).fetchall()
+#         marco = (data[:])
+#         print (marco)
+#         return marco
+# get_clients_data()
+
+def laptop_arif():
+    query_CPU= cur.execute("SELECT DateCreated, CPU FROM monitor WHERE Hostname='LAPTOP-ARIF'").fetchall()
+    query_MEM= cur.execute("SELECT DateCreated, Memory FROM monitor WHERE Hostname='LAPTOP-ARIF'").fetchall()
+    query_HDD= cur.execute("SELECT DateCreated, HDD FROM monitor WHERE Hostname='LAPTOP-ARIF'").fetchall()
+    CPU = [(elem1,elem2) for elem1, elem2 in query_CPU]
+    MEM = [(elem1,elem2) for elem1, elem2 in query_MEM]
+    HDD = [(elem1,elem2) for elem1, elem2 in query_HDD]
+
+    plt.plot(*zip(*CPU))
+    plt.title("LAPTOP-ARIF")
+    plt.show()
+
+    plt.plot(*zip(*MEM))
+    plt.title("LAPTOP-ARIF")
+    plt.show()
+
+    plt.plot(*zip(*HDD))
+    plt.title("LAPTOP-ARIF")
+    plt.show()
 
 
-def get_hostname():
-    hostnames = create_list()
-    for hostname in hostnames:
-        data = cur.execute("SELECT Hostname, CPU, Memory, HDD from monitor WHERE Hostname='%s'"%(hostname)).fetchall()
-        print (data)
-get_hostname()
 
+laptop_arif()
+
+
+
+# def laptop_arif():
+#     hostname= cur.execute("SELECT Hostname FROM monitor WHERE Hostname='LAPTOP-ARIF'").fetchone()
+#     arif_hostname = hostname[0]
+#     return arif_hostname
+#
+# def cpu_arif():
+#     CPU= cur.execute("SELECT CPU FROM monitor WHERE Hostname='LAPTOP-ARIF'").fetchall()
+#     arif_cpu = CPU[1:]
+#     return arif_cpu
+
+
+# arif_hostname = laptop_arif()
+# arif_cpu = cpu_arif()
+# print (arif_hostname)
+# print (arif_cpu)
 
 # class Monitoring():
 #     ''''This is the management script for the monitoring application'''
