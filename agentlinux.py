9aad35c197e54531907904c98e93a04d8d5efa46
akkermansie <arif_akkermans@hotmail.com>
2017-01-06 18:19:31 +0100

    uodate

---
+++ agentlinux.py
@@ -0,0 +1,28 @@
+try:
+    import pyodbc, psutil, socket, datetime, time
+except ImportError:
+    raise ImportError("Packages could not be imported")
+
+try:
+    con = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server}};SERVER=automation-maf.database.windows.net,1433', user='marif@automation-maf', password='P@ssw0rd', database='monitoring')
+    c=con.cursor()
+except Exception as db_connection:
+    print("Er gaat iets mis. Foutmelding: %s" %(db_connection))
+
+    # if sys.platform.startswith('linux'):
+    # elif sys.platform.startswith('win32'):
+
+def mon_loop():
+    for x in range(1):
+        hostname = (socket.gethostname())
+        cpu = (psutil.cpu_percent(interval=1))
+        memory = (psutil.virtual_memory().percent)
+        disk = (psutil.disk_usage('/').percent)
+        date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        c.execute("INSERT into monitor values(?,?,?,?,?)",hostname,cpu,memory,disk,date)
+        c.commit()
+        time.sleep(30)
+#c.execute("DROP table monitor")
+
+while True:
+    mon_loop()
