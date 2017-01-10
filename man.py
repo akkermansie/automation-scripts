38c2dae5eabf5e52461391bd9de6324799cb4465
akkermansie <arif_akkermans@hotmail.com>
2017-01-10 10:38:46 +0100

    Update man.py :+1:

--- man.py
+++ man.py
@@ -106,7 +106,7 @@ def linux_client():
     plt.clf()
 linux_client()
 
-
+### This function querys the database, creates a Matplotlib figure and saves it on the server ###
 def windows_client():
 # Database query
     query_CPU= cur.execute("SELECT TOP 10 DateCreated, CPU FROM monitor WHERE Hostname='win10-client' ORDER BY DateCreated DESC").fetchall()
@@ -168,97 +168,3 @@ def windows_client():
     plt.cla()
     plt.clf()
 windows_client()
-try:
-    import os, pyodbc, socket, matplotlib.pyplot as plt, matplotlib.dates as mdates
-except ImportError:
-    raise ImportError("Packages could not be imported")
-
-try:
-    con = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER=automation-maf.database.windows.net,1433', user='marif@automation-maf', password='P@ssw0rd', database='monitoring')
-    cur=con.cursor()
-except Exception as db_connection:
-    print("Er gaat iets mis. Foutmelding: %s" %(db_connection))
-
-dates = mdates.DateFormatter('%m-%d %H:%M:%S')
-
-def create_table():
-#CREATE DB TABLE
-    if cur.tables(table='monitor', tableType ='TABLE').fetchone():
-        print ("Table exists")
-    else:
-        data = ("""
-                          CREATE TABLE monitor
-                          (
-                          Hostname varchar(25),
-                          CPU int,
-                          Memory int,
-                          HDD int,
-                          DateCreated DATETIME,
-                          )
-                          """)
-        cur.execute(data)
-        cur.commit()
-        cur.close()
-        print ("Table created")
-create_table()
-
-def laptop_arif():
-# Database query
-    query_CPU= cur.execute("SELECT DateCreated, CPU FROM monitor WHERE Hostname='LAPTOP-ARIF'").fetchall()
-    query_MEM= cur.execute("SELECT DateCreated, Memory FROM monitor WHERE Hostname='LAPTOP-ARIF'").fetchall()
-    query_HDD= cur.execute("SELECT HDD FROM monitor WHERE Hostname='LAPTOP-ARIF' ORDER BY DateCreated DESC").fetchone()
-    Date_CPU = [x[0] for x in query_CPU]
-    CPU_Y = [y[1] for y in query_CPU]
-    Date_MEM = [x[0] for x in query_MEM]
-    MEM_Y = [y[1] for y in query_MEM]
-    HDD_STR = ("".join(map(str, query_HDD)))
-    HDD_INT = int(HDD_STR)
-
-# CPU GRAPH
-    cpu_loc = ('/home/admin/django/static/sitefiles/img/linux-client-cpu.png')
-    fig, ax = plt.subplots()
-    fig.suptitle("Laptop-Arif", fontsize=20)
-    fig.text(0.5, 0.010,'Timestamp',fontsize=20, ha='center')
-    fig.text(0.04, 0.5, 'CPU %Used', fontsize=20,va='center', rotation='vertical')
-    plt.plot(Date_CPU,CPU_Y)
-    ax.xaxis.set_major_formatter(dates)
-    plt.grid(True)
-    plt.ylim(0,100)
-    fig.autofmt_xdate()
-    os.remove(cpu_loc)
-    plt.savefig(cpu_loc)
-    plt.cla()
-    plt.clf()
-
-# MEMORY GRAPH
-    fig, ax = plt.subplots()
-    fig.suptitle("Laptop-Arif", fontsize=20)
-    fig.text(0.5, 0.010,'Timestamp',fontsize=20, ha='center')
-    fig.text(0.04, 0.5, 'Memory %Used', fontsize=20,va='center', rotation='vertical')
-    plt.plot(Date_MEM,MEM_Y)
-    ax.xaxis.set_major_formatter(dates)
-    plt.grid(True)
-    plt.ylim(0,100)
-    fig.autofmt_xdate()
-    plt.savefig('/home/admin/django/static/sitefiles/img/linux-client-memory.png')
-    plt.cla()
-    plt.clf()
-
-# HDD PIE CHART
-    labels = ('Used space', 'Free space')
-    sizes = (HDD_INT,100-HDD_INT)
-    plt.title("Laptop-Arif", fontsize=20)
-    colors = ('orange', 'yellowgreen')
-    plt.pie(sizes,
-        labels=labels,
-        colors=colors,
-        autopct='%1.1f%%',
-        shadow=True,
-        startangle=90
-        )
-    plt.axis('equal')
-    plt.tight_layout()
-    plt.savefig('/home/admin/django/static/sitefiles/img/linux-client-hdd.png')
-    plt.cla()
-    plt.clf()
-laptop_arif()
